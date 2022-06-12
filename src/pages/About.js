24cd57dd14cd2813f183c94dadfa6ae99390d877
Elizabeth Cucuzzella <elizabethcucuzzella@Elizabeths-MacBook-Pro-3.local>
2022-06-12 14:09:47 -0400

    My assignment

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profilePic from "./assets/profilePic.HEIC";
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              
+              alt="Profile Pic"
+              >src = {profilePic}</img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Elizabeth Cucuzzella</div>
+            <div className="brief_description">
+              I am a sophomore studying data science and mathematics at Tufts University.
+            </div>
+          </div>
+        </div>
+      </div> 
       </div>
     )
   }
